a6b40268fa487c0b76494cfe9e949faf5882aa54
Nick Tomlin <nick.tomlin@gmail.com>
2014-08-27 12:27:21 -0500

    Update example app

--- examples/app.js
+++ examples/app.js
@@ -1,5 +1,9 @@
 var React = require('react');
 var ReactTable = require('../index');
-var fixtureData = require('../__tests__/data');
+var fixtureData = require('../test/unit/data');
 
-React.renderComponent(ReactTable({data: fixtureData.data}), document.body);
+React.renderComponent(ReactTable({data: fixtureData.data,
+  columnDisplay: {
+    'name': 'Full Name'
+  }
+}), document.body);
