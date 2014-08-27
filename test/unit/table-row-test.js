c6a0981dd32280e99b51dcb11dab81762b360027
Nick Tomlin <nick.tomlin@gmail.com>
2014-08-27 08:12:37 -0500

    Add table row

---
+++ test/unit/table-row-test.js
@@ -0,0 +1,28 @@
+jest.dontMock('../../src/table-row');
+require('./spec-helper');
+
+describe('Table Row', function () {
+  var React = require('react/addons');
+  var TestUtils = React.addons.TestUtils;
+  var helper = require('./spec-helper');
+  var TableRow = require('../../src/table-row');
+  var render = helper.render.bind(null, TableRow);
+
+  function objectLength (obj) {
+    return Object.keys(obj).length;
+  }
+
+  var data = {
+    name: 'bob',
+    occupation: 'steward',
+    favoriteColor: 'blue'
+  };
+
+  it('creates a <td> for each element of supplied data object', function () {
+    var tr = render({data: data});
+    var expectedTds = objectLength(data);
+
+    var tds = TestUtils.scryRenderedDOMComponentsWithTag(tr, 'td');
+    expect(tds.length).toEqual(expectedTds);
+  });
+});
